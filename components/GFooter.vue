6d2414bf0dfd23ae1ee16a2c510b3d6dd73db5cb
dika <andikawpb200@gmail.com>
2023-01-30 12:18:25 +0700

    increase footer padding

--- components/GFooter.vue
+++ components/GFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="footer p-2">
+  <footer class="footer p-2 py-5">
     <div class="container">
       <div class="row">
         <div class="col-12 col-lg-4 text-center">
